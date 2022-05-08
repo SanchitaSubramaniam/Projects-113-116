27d3f379d05de1c2dfce1bb824b7f494fbe1d0d4
SanchitaSubramaniam <86506500+SanchitaSubramaniam@users.noreply.github.com>
2022-05-08 10:06:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload() {
+    mustache_nose = loadImage('https://i.postimg.cc/qvwnrSqC/handlebar-mustache-png-removebg-preview.png');
 }
 
 function setup() {
@@ -18,6 +22,7 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot() {
@@ -29,6 +34,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
